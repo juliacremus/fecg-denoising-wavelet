e7c52f6b3c640dbe619efcb0e558486306313df5
Julia Remus <100324382+juliacremus@users.noreply.github.com>
2023-08-30 11:31:57 -0300

    implements find peaks

--- denoising_analysis.py
+++ denoising_analysis.py
@@ -12,6 +12,7 @@ import mne
 import glob
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
 
 from haar_transform.haar_tranform import HaarTransform
 
@@ -24,10 +25,10 @@ def tresholding(N, details, level, type_threshold, type_thresholding):
         threshold = minimax_threshold(N, details)
     elif type_threshold == 'universal':
         threshold = universal_threshold(N, details)
-    elif type_threshold == 'universal':
+    elif type_threshold == 'han':
         threshold = han_etal_threshold(N, details, level)
     elif type_threshold == 'alice':
-        spc_shrink(details)
+        threshold = spc_shrink(details)
     
     if type_thresholding == 'hard':
         return_value = details
@@ -58,7 +59,7 @@ def universal_threshold(N, details):
     
     return threshold
 
-def han_etal_threshold(N, details, level, L):
+def han_etal_threshold(N, details, level, L=10):
     
     if level == 1:
         threshold = sigma(details) * np.sqrt(2 * np.log(N))
@@ -69,7 +70,7 @@ def han_etal_threshold(N, details, level, L):
     
     return threshold
 
-def spc_shrink(details, coef_d = 1.9086049):
+def spc_shrink(details, coef_d = 2):
     
     # padrao alpha = 1%
     
@@ -136,7 +137,7 @@ PATH = '/home/julia/Documents/temp_julia/denoising_analysis/data'
 
 f = 1 / 2
 
-levels = 3
+levels = 7
 
 #%% Get data 
 
@@ -178,30 +179,149 @@ N = len(data[0])
 #%%
 
 
-intervalo = 1024
+intervalo = 512
 
 #%% Transform data with wavelets
 
 haar = HaarTransform(data[0][0:intervalo], levels, f)
 
-foward_transform_array = haar.run_foward_transform()
+details = haar.run_cascade_multiresolution_transform()
 
 #%% Plot raw data 
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(2)
 
-ax.plot(times, data[0], label='Direct')
+ax[0].plot(data[0][:intervalo], label='Direct')
 
 # ax.plot(times[:intervalo], data[0][:intervalo], label='Direct')
 
-ax.plot(times[0:intervalo][::2], foward_transform_array[:int(intervalo/2)])
+ax[1].plot(details[:int(intervalo/2)])
 
-ax.grid()
+ax[0].grid()
 
-ax.legend()
+ax[0].legend()
 
-#%% Filter data method 1
+#%%
 
+data_filtered = tresholding(N, details, levels, 'alice', 'hard')
+
+#%%
+
+reconstruct_data = HaarTransform(data_filtered, levels, f)
+
+filtered_signal = reconstruct_data.run_cascade_multiresolution_inv_transform()
+#%% Plot raw data 
+
+fig, ax = plt.subplots(3)
+
+ax[0].plot(data[0][:intervalo], label='Direct')
+
+# ax.plot(times[:intervalo], data[0][:intervalo], label='Direct')
+
+ax[1].plot(details[:int(intervalo/2)])
+
+ax[2].plot(filtered_signal)
+
+ax[0].grid()
+
+ax[0].legend()
+
+#%%
+
+peaks, _ = find_peaks(np.abs(filtered_signal), height=0)
+
+print()
+
+#%%
+
+fig, ax = plt.subplots()
+
+ax.plot(peaks, filtered_signal[peaks], marker = 'x')
+
+ax.plot(filtered_signal)
+
+#%%
+
+norm_filtered_signal = filtered_signal / np.max(filtered_signal)
+
+linear_coef = np.array([0])
+
+for index in range(len(peaks) - 1):
+    
+    this_value = norm_filtered_signal[index]
+    next_value = norm_filtered_signal[index + 1]
+    
+    x = np.array([peaks[index], peaks[index + 1]])
+    y = np.array([this_value, next_value])
+    
+    A = np.vstack([x, np.ones(len(x))]).T
+
+    a, b = np.linalg.lstsq(A, y)[0]
+    
+    linear_coef = np.append(linear_coef, np.array([a]))
+    
+#%%
+
+fig, ax = plt.subplots()
+
+ax.plot(peaks, linear_coef)    
+
+ax.plot(peaks, filtered_signal[peaks], marker = 'x')    
+
+#%%
+# In the real dataset, where
+# mECG residuals could be present after the fECG extraction algo-
+# rithm, the QRS averaging involved only those complexes exhibiting
+# a Pearson’s correlation coeﬃcient above a given threshold, empiri-
+# cally chosen to be 0.6. If the number of correlated beats was lower
+# than four, the signal was treated as non-deterministic, then substi-
+# tuting Appf by the computation of four times the median standard
+# deviation of such beats.
+
+#%% get QRS complex in the wavelet denoising way
+
+# get peaks
+
+# in the interval of peak  +40 ms is considered to be QRS complex
+# outside of it calculates sigma of noise
+# if in complex calculates amplitude
+# and then calculates SNR
+
+# for accuracy calculation                      we have to get this code
+# https://iopscience.iop.org/article/10.1088/0967-3334/35/8/1569/pdf
+# but its on matlab  grrr
+
+#%%
+
+def get_peaks(data, PEARSON_THRESHOLD = 0.6):
+    
+    # Get peaks
+    
+    # Calculate linear correlation of peaks (lower and up)
+    
+    # If correlation is bigger than threshold than is consider a qrs complex
+    
+    peaks, _ = find_peaks(data, height=0)
+    
+    print()
+    
+    return peaks
+
+
+def calculate_snr():
+    
+    
+    # If interval is qrs complex calculates App
+    
+    # Else calculates sigma of noise
+    
+    
+
+    # maybe we have to calculate the median value of SNR
+    # in the whole time data
+    # and plot comparisson between mean values with diffente thheshold methossss
+    
+    return 
 #%% #%% Filter data method 2
 
 
